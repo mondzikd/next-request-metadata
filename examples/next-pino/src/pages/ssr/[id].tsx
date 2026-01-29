863ebb5df33befeb2ec5aa23b7473e5c893984d2
Dominik <mondzik.d@gmail.com>
2026-01-29 21:05:08 +0100

    Remove dependency on Next.js

--- examples/next-pino/src/pages/ssr/[id].tsx
+++ examples/next-pino/src/pages/ssr/[id].tsx
@@ -1,34 +1,60 @@
 import { calculateSomeProp } from "@/lib/helperFunction";
-import { InferGetServerSidePropsType, NextPage } from "next";
+import {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage,
+} from "next";
 import { metadataRequestWrapper } from "../../../../../src";
-
-type SSRExamplePageProps = {
-  someProp: string;
-};
 
 /**
  * Example of a Next.js page that uses getServerSideProps with metadata logging on the server.
  *
  * Because of metadataRequestWrapper we don't have to manually pass down metadata to invoked functions.
  */
-export const getServerSideProps = metadataRequestWrapper<SSRExamplePageProps>(
-  async (context) => {
-    const id = context.params?.id;
+export const getServerSideProps = metadataRequestWrapper(async (context) => {
+  const id = context.params?.id;
 
-    if (typeof id !== "string") {
-      return {
-        notFound: true,
-      };
-    }
-
-    // Call some function that uses logging. Don't need to pass metadata down.
-    const someProp = calculateSomeProp(id);
-
+  if (typeof id !== "string") {
     return {
-      props: { someProp },
+      notFound: true,
     };
-  },
-);
+  }
+
+  // Call some function that uses logging. Don't need to pass metadata down.
+  const someProp = calculateSomeProp(id);
+
+  return {
+    props: { someProp },
+  };
+});
+
+// type SSRExamplePageProps = {
+//   someProp: string;
+// };
+
+// Alternative: Using explicit typing for original getServerSideProps
+// const originalGetServerSideProps: GetServerSideProps<
+//   SSRExamplePageProps
+// > = async (context) => {
+//   const id = context.params?.id;
+
+//   if (typeof id !== "string") {
+//     return {
+//       notFound: true,
+//     };
+//   }
+
+//   // Call some function that uses logging. Don't need to pass metadata down.
+//   const someProp = calculateSomeProp(id);
+
+//   return {
+//     props: { someProp },
+//   };
+// };
+
+// export const getServerSideProps = metadataRequestWrapper(
+//   originalGetServerSideProps,
+// );
 
 export const SSRExamplePage: NextPage<
   InferGetServerSidePropsType<typeof getServerSideProps>
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
-import type { GetServerSideProps, GetServerSidePropsContext } from "next";
 import { AsyncLocalStorage } from "node:async_hooks";
 import { randomUUID } from "node:crypto";
 
 export type Metadata = Record<string, string | number | boolean>;
 
+type MetadataRequestWrapper = <Args extends any[], R>(
+  original: (...args: Args) => R,
+  prepareMetadata?: (arg: Args[0]) => Metadata,
+) => (...args: Args) => R;
+
+type DefaultMinimalContext = {
+  req: { headers: Record<string, string> };
+  res: { setHeader: (key: string, value: string) => void };
+};
+
 const asyncLocalStorage = new AsyncLocalStorage<Metadata>();
 
-const getRequestId = (context: GetServerSidePropsContext): string => {
+const getRequestId = (context: DefaultMinimalContext): string => {
   const requestId = context.req.headers["x-request-id"];
 
   if (typeof requestId === "string") {
@@ -20,9 +29,7 @@ const getRequestId = (context: GetServerSidePropsContext): string => {
  * Prepares example metadata with x-request-id only.
  * Also showcases passing metadata to response headers.
  */
-const prepareMetadataDefault = (
-  context: GetServerSidePropsContext,
-): Metadata => {
+const prepareMetadataDefault = (context: DefaultMinimalContext): Metadata => {
   const requestId = getRequestId(context);
 
   context.res.setHeader("x-request-id", requestId);
@@ -33,19 +40,19 @@ const prepareMetadataDefault = (
 /**
  * Next.js getServerSideProps wrapper to share metadata context across nested functions.
  *
- * @param getServerSideProps original getServerSideProps functions, which nested functions will share metadata context
- * @param prepareMetadata function preparing metadata for that will be shared in the context
- * @returns original getServerSideProps wrapped with metadata context
+ * @param original getServerSideProps function, that nested functions will share metadata context.
+ * @param prepareMetadata function preparing metadata, that will be shared in the context. Context
+ *                        (first argument) passed to original function will be passed to prepareMetadata.
+ *                        Defaults to storing x-request-id AND also injecting it to context.res.headers.
+ * @returns original getServerSideProps wrapped with metadata context.
  */
-export const metadataRequestWrapper = <T extends { [key: string]: any }>(
-  getServerSideProps: GetServerSideProps<T>,
-  prepareMetadata: (
-    context: GetServerSidePropsContext,
-  ) => Metadata = prepareMetadataDefault,
-): GetServerSideProps<T> => {
-  return (context) => {
-    const store = prepareMetadata(context);
-    return asyncLocalStorage.run(store, getServerSideProps, context);
+export const metadataRequestWrapper: MetadataRequestWrapper = (
+  original,
+  prepareMetadata = prepareMetadataDefault,
+) => {
+  return (...args) => {
+    const store = prepareMetadata(args[0]);
+    return asyncLocalStorage.run(store, original, ...args);
   };
 };
 

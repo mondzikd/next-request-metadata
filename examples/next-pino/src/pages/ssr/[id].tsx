3fd9bfcf43ab908cd35755d4d11400b2e9d3458d
Dominik <mondzik.d@gmail.com>
2026-03-01 12:37:04 +0100

    Add details to readme

--- examples/next-pino/src/pages/ssr/[id].tsx
+++ examples/next-pino/src/pages/ssr/[id].tsx
@@ -1,41 +1,20 @@
 import { calculateSomeProp } from "@/lib/helperFunction";
 import {
   GetServerSideProps,
-  GetServerSidePropsContext,
   InferGetServerSidePropsType,
   NextPage,
 } from "next";
 import { logger, logMetadataRequestWrapper } from "@/lib/logger";
+
+type SSRExamplePageProps = {
+  someProp: string;
+};
 
 /**
  * Example of a Next.js page that uses getServerSideProps with metadata logging on the server.
  *
  * Because of logMetadataRequestWrapper we don't have to manually pass down metadata to invoked functions.
  */
-// export const getServerSideProps = logMetadataRequestWrapper(async (context: GetServerSidePropsContext<{id: string}>) => {
-//   const id = context.params?.id;
-
-//   logger.info({ msg: `Received request for ID: ${id}` });
-
-//   if (typeof id !== "string") {
-//     return {
-//       notFound: true,
-//     };
-//   }
-
-//   // Call some function that uses logging. Don't need to pass metadata down.
-//   const someProp = calculateSomeProp(id);
-
-//   return {
-//     props: { someProp },
-//   };
-// });
-
-type SSRExamplePageProps = {
-  someProp: string;
-};
-
-// Alternative: Using explicit typing for original getServerSideProps
 const originalGetServerSideProps: GetServerSideProps<
   SSRExamplePageProps
 > = async (context) => {
